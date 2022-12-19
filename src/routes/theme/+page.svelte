c2a9a1e749a1d1b5038bf771a7d7a41b419c3f09
Kendell R <KTibow@users.noreply.github.com>
2022-12-19 11:39:28 -0800

    add theme exporter

--- src/routes/theme/+page.svelte
+++ src/routes/theme/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { browser } from "$app/environment";
+  import Button from "$lib/buttons/Button.svelte";
   import {
     argbFromHex,
     hexFromArgb,
@@ -31,25 +32,105 @@
     bg: colors[bg as keyof Scheme] as number,
     fg: colors[fg as keyof Scheme] as number,
   });
+  const genCSS = () => {
+    const genColorVariable = (name: string, argb: number) => {
+      const kebabCase = name.replace(/[A-Z]/g, (letter: string) => `-${letter.toLowerCase()}`);
+      const red = (argb >> 16) & 255;
+      const green = (argb >> 8) & 255;
+      const blue = argb & 255;
+      return `--md-sys-color-${kebabCase}: ${red} ${green} ${blue};`;
+    };
+    const lightColors = Object.entries(theme.schemes.light.toJSON())
+      .map((colorInfo) => genColorVariable(...colorInfo))
+      .join("\n");
+    const darkColors = Object.entries(theme.schemes.dark.toJSON())
+      .map((colorInfo) => genColorVariable(...colorInfo))
+      .join("\n");
+    const colors = `@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+@media (prefers-color-scheme: light) {
+:root {
+${lightColors}
+}
+}
+@media (prefers-color-scheme: dark) {
+:root {
+${darkColors}
+}
+}
+:root {
+--md-sys-elevation-0: none;
+--md-sys-elevation-1: 0px 3px 1px -2px rgb(var(--md-sys-color-shadow) / 0.2),
+0px 2px 2px 0px rgb(var(--md-sys-color-shadow) / 0.14),
+0px 1px 5px 0px rgb(var(--md-sys-color-shadow) / 0.12);
+--md-sys-elevation-2: 0px 2px 4px -1px rgb(var(--md-sys-color-shadow) / 0.2),
+0px 4px 5px 0px rgb(var(--md-sys-color-shadow) / 0.14),
+0px 1px 10px 0px rgb(var(--md-sys-color-shadow) / 0.12);
+--md-sys-elevation-3: 0px 5px 5px -3px rgb(var(--md-sys-color-shadow) / 0.2),
+0px 8px 10px 1px rgb(var(--md-sys-color-shadow) / 0.14),
+0px 3px 14px 2px rgb(var(--md-sys-color-shadow) / 0.12);
+--md-sys-elevation-4: 0px 5px 5px -3px rgb(var(--md-sys-color-shadow) / 0.2),
+0px 8px 10px 1px rgb(var(--md-sys-color-shadow) / 0.14),
+0px 3px 14px 2px rgb(var(--md-sys-color-shadow) / 0.12);
+--md-sys-elevation-5: 0px 8px 10px -6px rgb(var(--md-sys-color-shadow) / 0.2),
+0px 16px 24px 2px rgb(var(--md-sys-color-shadow) / 0.14),
+0px 6px 30px 5px rgb(var(--md-sys-color-shadow) / 0.12);
+}
+.md-background {
+background-color: rgb(var(--md-sys-color-background));
+color: rgb(var(--md-sys-color-on-background));
+}
+.md-label-large {
+font-family: Roboto, system-ui, sans-serif;
+font-weight: 500;
+font-size: 14px;
+line-height: 20px;
+letter-spacing: 0.1px;
+}
+.md-label-medium {
+font-family: Roboto, system-ui, sans-serif;
+font-weight: 500;
+font-size: 12px;
+line-height: 16px;
+letter-spacing: 0.5px;
+}
+.md-body-medium {
+font-family: Roboto, system-ui, sans-serif;
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+letter-spacing: 0.25px;
+}
+* {
+box-sizing: border-box;
+}`;
+    const file = new File([colors], "theme.css", {
+      type: "text/plain",
+    });
+    const url = window.URL.createObjectURL(file);
+    const link = document.createElement("a");
+    link.setAttribute("download", "theme.css");
+    link.setAttribute("href", url);
+    document.head.appendChild(link);
+    link.click();
+    link.remove();
+  };
 </script>
 
 <svelte:head>
   <title>M3 Svelte: Theme</title>
 </svelte:head>
 <h1 class="md-display-large">Theme</h1>
-<p>
-  <label>
-    Source color <button
-      style="background-color: {browser && hexFromArgb(sourceColor)};"
-      class="colorDisc"
-      on:click={() => sourceColorInput.click()}
-    />
-    <input
-      type="color"
-      bind:this={sourceColorInput}
-      on:change={() => (sourceColor = argbFromHex(sourceColorInput.value))}
-    />
-  </label>
+<p class="sourceChooser">
+  Source color <button
+    style="background-color: {browser && hexFromArgb(sourceColor)};"
+    class="colorDisc"
+    on:click={() => sourceColorInput.click()}
+  />
+  <input
+    type="color"
+    bind:this={sourceColorInput}
+    on:change={() => (sourceColor = argbFromHex(sourceColorInput.value))}
+  />
   <input
     type="file"
     bind:files={sourceFile}
@@ -65,6 +146,9 @@
       reader.readAsDataURL(sourceFile[0]);
     }}
   />
+  {#if theme}
+    <Button type="filled" on:click={genCSS}>Export theme</Button>
+  {/if}
 </p>
 {#if theme}
   {#each Object.entries({ Light: theme.schemes.light, Dark: theme.schemes.dark }) as [name, colors]}
@@ -78,11 +162,10 @@
       {/each}
     </div>
   {/each}
-  <p>Exporting soon</p>
 {/if}
 
 <style>
-  label {
+  .sourceChooser {
     display: flex;
     align-items: center;
     gap: 0.5rem;
