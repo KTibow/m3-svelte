be4fe96b7301e7022b987d61f88a5efd93dfac88
Kendell R <KTibow@users.noreply.github.com>
2022-12-24 11:26:50 -0800

    bump version

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,9 @@
 export { default as Button } from "./buttons/Button.svelte";
 export { default as FAB } from "./buttons/FAB.svelte";
 export { default as SegmentedButton } from "./buttons/SegmentedButton.svelte";
+
 export { default as Card } from "./containers/Card.svelte";
+
 export { default as Checkbox } from "./forms/Checkbox.svelte";
 export { default as Chip } from "./forms/Chip.svelte";
 export { default as CircularProgress } from "./forms/CircularProgress.svelte";
@@ -10,5 +12,6 @@ export { default as Radio } from "./forms/Radio.svelte";
 export { default as Slider } from "./forms/Slider.svelte";
 export { default as Switch } from "./forms/Switch.svelte";
 export { default as TextField } from "./forms/TextField.svelte";
+
 export { default as NavDrawer } from "./nav/NavDrawer.svelte";
 export { default as NavRail } from "./nav/NavRail.svelte";
