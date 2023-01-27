81426e07ab1827cfad686a5f938528ad4d25ab82
Kendell R <KTibow@users.noreply.github.com>
2023-01-26 19:39:37 -0800

    recode text fields and update theme

--- src/lib/forms/TextField.svelte
+++ src/lib/forms/TextField.svelte
@@ -3,28 +3,22 @@
   import iconError from "@iconify-icons/ic/error-outline";
   export let display = "inline-flex";
   export let inputStyle: "filled" | "outlined";
+  export let error = false;
   export let icon: IconifyIcon | null = null;
   export let name: string;
-  export let value = "";
-  export let error = false;
   export let supportingText: null | string = null;
-  let focused: boolean;
+  export let value = "";
   let id = `input-${crypto.randomUUID()}`;
 </script>
 
-<div class:error>
-  <div class="m3-container type-{inputStyle}" class:has-icon={icon} style="display: {display};">
-    <input
-      on:focus={() => (focused = true)}
-      on:blur={() => (focused = false)}
-      bind:value
-      required
-      class="md-body-large"
-      class:value
-      {id}
-      {...$$props}
-    />
-    <div class="layer" />
+<div>
+  <div
+    class="m3-container type-{inputStyle}"
+    class:has-icon={icon}
+    class:error
+    style="display: {display}"
+  >
+    <input bind:value required {id} {...$$props} class="md-body-large" class:value />
     {#if icon}
       <span class="leadingIcon">
         <Icon {icon} />
@@ -35,13 +29,11 @@
         <Icon icon={iconError} />
       </span>
     {/if}
-    <label for={id} class={focused || value ? "md-body-small" : "md-body-large"}>{name}</label>
-    <fieldset>
-      <legend class="md-body-small">{name}</legend>
-    </fieldset>
+    <div class="layer" />
+    <label class="md-body-large" for={id}>{name}</label>
   </div>
   {#if supportingText}
-    <p class="supporting md-body-small">{supportingText}</p>
+    <p class="supporting md-body-small" class:error>{supportingText}</p>
   {/if}
 </div>
 
@@ -56,123 +48,31 @@
     width: 1.5rem;
     height: 1.5rem;
   }
-  .leadingIcon,
-  .trailingIcon {
-    display: inline-flex;
-    pointer-events: none;
-  }
-  .leadingIcon {
-    margin: 0 1rem 0 0.75rem;
-    align-self: center;
-  }
-  .trailingIcon {
+  input {
     position: absolute;
-    right: 0.75rem;
-    top: 50%;
-    transform: translate(0, -50%);
-    color: rgb(var(--error));
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0 1rem;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    color: rgb(var(--md-sys-color-on-surface));
   }
   .layer {
     position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    transition: all 150ms;
+    inset: 0;
+    transition: all 200ms;
     border-radius: inherit;
     pointer-events: none;
-  }
-  input {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 100%;
-    top: 0;
-    bottom: 0;
-    height: 100%;
-    background-color: transparent;
-    border: none;
-    color: rgb(var(--md-sys-color-on-surface));
-    outline: none;
-    padding: 0 1rem;
   }
   label {
+    position: absolute;
     left: 1rem;
-    top: 50%;
-    transform: translate(0, -50%);
-    position: absolute;
-    transition: all 300ms;
+    top: 1rem;
+    transition: all 200ms, font 400ms;
     pointer-events: none;
-    color: rgb(var(--error));
-  }
-  legend {
-    opacity: 0;
-  }
-  fieldset {
-    position: absolute;
-    top: -0.5rem;
-    bottom: -1px;
-    left: -1px;
-    right: -1px;
-    margin: 0;
-    padding: 0 0.75rem;
-    border-radius: inherit;
-    color: rgb(var(--error, var(--md-sys-color-outline)));
-    border: solid 2px currentColor;
-    opacity: 0;
-    transition: all 300ms;
-    pointer-events: none;
-  }
-  .supporting {
-    color: rgb(var(--md-sys-color-on-surface-variant));
-    margin: 0.25rem 0 0 1rem;
-  }
-  .type-filled {
-    background-color: rgb(var(--md-sys-color-surface-variant));
-    border-radius: 0.25rem 0.25rem 0 0;
-  }
-  .type-filled > .layer {
-    border-bottom: solid 1px rgb(var(--error, var(--md-sys-color-on-surface-variant)));
-  }
-  .type-filled:focus-within > .layer {
-    border-bottom: solid 2px rgb(var(--error, var(--md-sys-color-primary)));
-  }
-  .type-filled:focus-within > label {
-    color: rgb(var(--error, var(--md-sys-color-primary)));
-  }
-  .type-filled:hover > .layer {
-    background-color: rgb(var(--md-sys-color-on-surface) / 0.08);
-  }
-  .type-filled > input:is(:focus, .value, :required:valid) {
-    padding-top: 1.25rem;
-    padding-bottom: 0.5rem;
-  }
-  .type-outlined {
-    border-radius: 0.25rem;
-  }
-  .type-outlined > .layer {
-    border: solid 1px rgb(var(--error, var(--md-sys-color-outline)));
-  }
-  .type-outlined > input:is(:focus, .value, :required:valid) + .layer {
-    border: none;
-  }
-  .type-outlined > input:is(:focus, .value, :required:valid) ~ fieldset {
-    opacity: 1;
-  }
-  .type-outlined > input:is(:focus, .value, :required:valid) ~ label {
-    left: 15px;
-    top: -0.5rem;
-    transform: none;
-  }
-  .type-outlined:hover > .layer {
-    border-color: rgb(var(--error, var(--md-sys-color-on-surface)));
-  }
-  .type-outlined:hover > fieldset,
-  .type-outlined:hover > label {
-    color: rgb(var(--error, var(--md-sys-color-on-surface)));
-  }
-  .type-outlined:focus-within > fieldset {
-    color: rgb(var(--error, var(--md-sys-color-primary)));
+    color: rgb(var(--error, currentColor));
   }
   .error {
     --error: var(--md-sys-color-error);
@@ -180,8 +80,73 @@
   .error:hover {
     --error: var(--md-sys-color-on-error-container);
   }
-  .error > .supporting {
-    color: rgb(var(--md-sys-color-error));
+  .supporting {
+    color: rgb(var(--md-sys-color-on-surface-variant));
+    margin: 0.25rem 0 0 0;
+    padding: 0 1rem;
+  }
+  .supporting.error {
+    color: var(--md-sys-color-error);
+  }
+
+  .type-filled {
+    background-color: rgb(var(--md-sys-color-surface-variant));
+    border-radius: 0.25rem 0.25rem 0 0;
+  }
+  .type-filled > .layer {
+    color: rgb(var(--error, var(--md-sys-color-on-surface-variant)));
+    border-bottom: solid 1px currentColor;
+  }
+  .type-filled:hover > .layer {
+    background-color: rgb(var(--md-sys-color-on-surface) / 0.08);
+  }
+  .type-filled > input {
+    padding-top: 1.25rem;
+    padding-bottom: 0.5rem;
+  }
+  .type-filled input:is(:focus, .value, :required:valid) ~ label {
+    top: 0.5rem;
+  }
+
+  .type-outlined {
+    border-radius: 0.25rem;
+  }
+  .type-outlined > .layer {
+    color: rgb(var(--error, var(--md-sys-color-outline)));
+    border: solid 1px currentColor;
+  }
+  .type-outlined input:is(:focus, .value, :required:valid) ~ label {
+    top: -0.5rem;
+    left: 0.75rem;
+    padding: 0 0.25rem;
+    background-color: rgb(var(--md-sys-color-surface));
+  }
+
+  .m3-container:hover > :is(label, .layer) {
+    color: rgb(var(--error, var(--md-sys-color-on-surface)));
+  }
+  .m3-container:focus-within > :is(label, .layer) {
+    border-width: 2px;
+    color: rgb(var(--error, var(--md-sys-color-primary)));
+  }
+  input:is(:focus, .value, :required:valid) ~ label {
+    font-size: var(--md-sys-typescale-body-small-size, 12px);
+    line-height: var(--md-sys-typescale-body-small-height, 16px);
+    letter-spacing: var(--md-sys-typescale-body-small-tracking, 0.4);
+  }
+
+  .leadingIcon,
+  .trailingIcon {
+    display: inline-flex;
+    pointer-events: none;
+    align-self: center;
+  }
+  .leadingIcon {
+    margin: 0 1rem 0 0.75rem;
+  }
+  .trailingIcon {
+    margin: 0 0.75rem 0 auto;
+    color: rgb(var(--error));
   }
   .has-icon > input {
     padding-left: 3.25rem;
@@ -189,8 +154,7 @@
   .has-icon > label {
     left: 3.25rem;
   }
-  input:is(:focus, .value, :required:valid) ~ label {
-    top: 0.5rem;
-    transform: none;
+  .error > input {
+    padding-right: 3.25rem;
   }
 </style>
