2b4e8e1a2838d93033024c15cbab5fdfa64aa84e
JL102 <jordanlees@mailbox.org>
2023-01-27 00:03:27 -0500

    Fixed tabbing

--- src/lib/utils/animation.ts
+++ src/lib/utils/animation.ts
@@ -116,30 +116,30 @@ export const slideFade = (_: Element, options: transitionOptions & slideFadeOpti
   const startOpacity = options.opacity || 0;
   
   const style = getComputedStyle(_);
-	const endOpacity = +style.opacity;
-	const height = parseFloat(style.height);
-	const padding_top = parseFloat(style.paddingTop);
-	const padding_bottom = parseFloat(style.paddingBottom);
-	const margin_top = parseFloat(style.marginTop);
-	const margin_bottom = parseFloat(style.marginBottom);
-	const border_top_width = parseFloat(style.borderTopWidth);
-	const border_bottom_width = parseFloat(style.borderBottomWidth);
+  const endOpacity = +style.opacity;
+  const height = parseFloat(style.height);
+  const padding_top = parseFloat(style.paddingTop);
+  const padding_bottom = parseFloat(style.paddingBottom);
+  const margin_top = parseFloat(style.marginTop);
+  const margin_bottom = parseFloat(style.marginBottom);
+  const border_top_width = parseFloat(style.borderTopWidth);
+  const border_bottom_width = parseFloat(style.borderBottomWidth);
   console.log(options.easing);
-	return {
+  return {
     delay: options.delay,
     duration: options.duration || 300,
     easing: options.easing || cubicOut,
-		css: t =>
-			'overflow: hidden;' +
-			`opacity: ${startOpacity + (endOpacity - startOpacity) * t};` + 
-			`height: ${t * height}px;` +
-			`padding-top: ${t * padding_top}px;` +
-			`padding-bottom: ${t * padding_bottom}px;` +
-			`margin-top: ${t * margin_top}px;` +
-			`margin-bottom: ${t * margin_bottom}px;` +
-			`border-top-width: ${t * border_top_width}px;` +
-			`border-bottom-width: ${t * border_bottom_width}px;`
-	};
+    css: t =>
+      'overflow: hidden;' +
+      `opacity: ${startOpacity + (endOpacity - startOpacity) * t};` + 
+      `height: ${t * height}px;` +
+      `padding-top: ${t * padding_top}px;` +
+      `padding-bottom: ${t * padding_bottom}px;` +
+      `margin-top: ${t * margin_top}px;` +
+      `margin-bottom: ${t * margin_bottom}px;` +
+      `border-top-width: ${t * border_top_width}px;` +
+      `border-bottom-width: ${t * border_bottom_width}px;`
+  };
 }
 
 interface heightOptions {
