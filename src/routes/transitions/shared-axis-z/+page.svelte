30e791b0eda6385af72ee6d293d0c6fc1b5c03b4
Kendell R <KTibow@users.noreply.github.com>
2023-02-13 07:00:20 -0800

    transition demos

---
+++ src/routes/transitions/shared-axis-z/+page.svelte
@@ -0,0 +1,92 @@
+<script>
+  import { sharedAxisTransition } from "$lib";
+  import Button from "$lib/buttons/Button.svelte";
+
+  let page = 0;
+</script>
+
+<main>
+  {#if page == 0}
+    <div
+      in:sharedAxisTransition={{ direction: "Z", leaving: false }}
+      out:sharedAxisTransition={{ direction: "Z", leaving: true }}
+    >
+      <p>
+        <Button type="outlined" on:click={() => (page = 1)}>next</Button>
+      </p>
+      <img
+        src="https://source.unsplash.com/192x168/?red"
+        alt="Red placeholder"
+        width="192"
+        height="168"
+      />
+    </div>
+  {:else if page == 1}
+    <div
+      in:sharedAxisTransition={{ direction: "Z", leaving: false }}
+      out:sharedAxisTransition={{ direction: "Z", leaving: true }}
+    >
+      <p>
+        <Button type="outlined" on:click={() => (page = 0)}>back</Button>
+        <Button type="outlined" on:click={() => (page = 2)}>next</Button>
+      </p>
+      <img
+        src="https://source.unsplash.com/192x168/?orange"
+        alt="Orange placeholder"
+        width="192"
+        height="168"
+      />
+    </div>
+  {:else if page == 2}
+    <div
+      in:sharedAxisTransition={{ direction: "Z", leaving: false }}
+      out:sharedAxisTransition={{ direction: "Z", leaving: true }}
+    >
+      <p>
+        <Button type="outlined" on:click={() => (page = 1)}>back</Button>
+        <Button type="outlined" on:click={() => (page = 3)}>next</Button>
+      </p>
+      <img
+        src="https://source.unsplash.com/192x168/?yellow"
+        alt="Yellow placeholder"
+        width="192"
+        height="168"
+      />
+    </div>
+  {:else}
+    <div
+      in:sharedAxisTransition={{ direction: "Z", leaving: false }}
+      out:sharedAxisTransition={{ direction: "Z", leaving: true }}
+    >
+      <p>
+        <Button type="outlined" on:click={() => (page = 2)}>back</Button>
+      </p>
+      <img
+        src="https://source.unsplash.com/192x168/?green"
+        alt="Green placeholder"
+        width="192"
+        height="168"
+      />
+    </div>
+  {/if}
+</main>
+
+<style>
+  main {
+    position: relative;
+    min-height: 100%;
+    overflow: hidden;
+  }
+  main > * {
+    position: absolute;
+    background-color: rgb(var(--m3-scheme-surface));
+    overflow: hidden;
+    width: 100%;
+    min-height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
