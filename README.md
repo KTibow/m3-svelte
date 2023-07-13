9ebb46c601038e5330ff618a6130670639849c2d
Kendell R <KTibow@users.noreply.github.com>
2023-07-13 14:41:13 -0700

    readme/workflows/exports

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # m3-svelte
 
-An attempt to make Material 3 in Svelte. See the [website](https://ktibow.github.io/m3-svelte/) for docs.
-
-## Getting started
-
-1. Install the library into a Svelte project (`npm i m3-svelte`).
-2. Use the [theme page](https://ktibow.github.io/m3-svelte/theme) to construct a theme. Copy the usage, and paste it in `+layout.svelte` (don't forget to import it first).
-3. Set up your typography. By default, M3 Svelte uses Roboto, but you need to import it from `https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap`. You could also customize the font (see [BaseStyles.svelte](https://github.com/KTibow/m3-svelte/blob/main/src/lib/colors/BaseStyles.svelte)).
-4. Set up your body to use the theme's background and text color, as well as its font. See [M3 Svelte's app.html](https://github.com/KTibow/m3-svelte/blob/main/src/app.html) for an example.
+M3 Svelte implements the Material 3 design system in Svelte. See the [website](https://ktibow.github.io/m3-svelte/) for demos and usage instructions.
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 export * from "./misc/utils.js";
-export { default as BaseStyles } from "./misc/BaseStyles.svelte";
 export { default as StyleFromScheme } from "./misc/StyleFromScheme.svelte";
 export * from "./misc/animation.js";
 export * from "./misc/easing.js";
@@ -18,23 +17,30 @@ export { default as Dialog } from "./containers/Dialog.svelte";
 // export { default as Menu } from "./containers/Menu.svelte";
 // export { default as Snackbar } from "./containers/Snackbar.svelte";
 
-// export { default as Checkbox } from "./forms/Checkbox.svelte";
-// export { default as Chip } from "./forms/Chip.svelte";
-// export { default as CircularProgress } from "./forms/CircularProgress.svelte";
-// export { default as DatePickerDocked } from "./forms/DatePickerDocked.svelte";
-// export { default as LinearProgress } from "./forms/LinearProgress.svelte";
-// export { default as Radio } from "./forms/Radio.svelte";
-// export { default as Slider } from "./forms/Slider.svelte";
-// export { default as Switch } from "./forms/Switch.svelte";
-// export { default as TextField } from "./forms/TextField.svelte";
+export { default as LinearProgress } from "./forms/LinearProgress.svelte";
+export { default as CircularProgress } from "./forms/CircularProgress.svelte";
+export { default as RadioAnim1 } from "./forms/RadioAnim1.svelte";
+export { default as RadioAnim2 } from "./forms/RadioAnim2.svelte";
+export { default as RadioAnim3 } from "./forms/RadioAnim3.svelte";
+export { default as Checkbox } from "./forms/Checkbox.svelte";
+export { default as CheckboxAnim } from "./forms/CheckboxAnim.svelte";
+export { default as Switch } from "./forms/Switch.svelte";
+export { default as Chip } from "./forms/Chip.svelte";
 
-// export { default as Divider } from "./utils/Divider.svelte";
+export { default as TabsPrimary } from "./nav/TabsPrimary.svelte";
+export { default as TabsPrimaryAnim } from "./nav/TabsPrimaryAnim.svelte";
+export { default as TabsSecondary } from "./nav/TabsSecondary.svelte";
+export { default as TabsSecondaryAnim } from "./nav/TabsSecondaryAnim.svelte";
+
+// export { default as DatePickerDocked } from "./forms/DatePickerDocked.svelte";
+// export { default as Slider } from "./forms/Slider.svelte";
+// export { default as TextField } from "./forms/TextField.svelte";
 
 // export { default as NavDrawer } from "./nav/NavDrawer.svelte";
 // export { default as NavRail } from "./nav/NavRail.svelte";
-// export { default as Tabs } from "./nav/Tabs.svelte";
 
 // export { default as ChipChooser } from "./utils/ChipChooser.svelte";
 // export { default as DateFieldAndPicker } from "./utils/DateFieldAndPicker.svelte";
+// export { default as Divider } from "./utils/Divider.svelte";
 // export { default as ResponsiveLayout } from "./utils/ResponsiveLayout.svelte";
 // export { default as SnackbarPlacer } from "./utils/SnackbarPlacer.svelte";
