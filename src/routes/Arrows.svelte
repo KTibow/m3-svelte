f27af78fa4c8bbbd87e30abede4102de157c206d
Kendell R <KTibow@users.noreply.github.com>
2023-07-16 15:40:26 -0700

    update package, arrows

--- src/routes/Arrows.svelte
+++ src/routes/Arrows.svelte
@@ -12,8 +12,8 @@
 </script>
 
 <div class="container">
-  <button class="m3-font-label-large" on:click={left}>&lt;</button>
-  <button class="m3-font-label-large" on:click={right}>&gt;</button>
+  <button class="m3-font-label-large left" on:click={left}>&lt;</button>
+  <button class="m3-font-label-large right" on:click={right}>&gt;</button>
 </div>
 
 <style>
@@ -25,13 +25,20 @@
   }
   button {
     flex: 1;
-    padding: 0;
+    font-size: 1rem;
     min-width: 1.5rem;
     height: 2rem;
+    padding: 0;
+
     border: none;
     background-color: rgb(var(--m3-scheme-primary));
     color: rgb(var(--m3-scheme-on-primary));
-    border-radius: 0.5rem;
     cursor: pointer;
+  }
+  button.left {
+    border-radius: 0.75rem 0.25rem 0.25rem 0.75rem;
+  }
+  button.right {
+    border-radius: 0.25rem 0.75rem 0.75rem 0.25rem;
   }
 </style>
