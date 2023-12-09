8fa9b19197bfba2ca73f64c45c719246deba5a01
Kendell R <KTibow@users.noreply.github.com>
2023-12-09 08:03:42 -0800

    rerun prettier

--- src/lib/forms/TextFieldMultiline.svelte
+++ src/lib/forms/TextFieldMultiline.svelte
@@ -72,8 +72,7 @@
     border: none;
     outline: none;
     padding: 1.5rem 1rem 0.5rem 1rem;
-    border-radius: var(--m3-textfield-filled-shape)
-      var(--m3-textfield-filled-shape) 0 0;
+    border-radius: var(--m3-textfield-filled-shape) var(--m3-textfield-filled-shape) 0 0;
     background-color: rgb(var(--m3-scheme-surface-container-highest));
     color: rgb(var(--m3-scheme-on-surface));
     resize: none;
@@ -93,8 +92,7 @@
   .layer {
     position: absolute;
     inset: 0;
-    border-radius: var(--m3-textfield-filled-shape)
-      var(--m3-textfield-filled-shape) 0 0;
+    border-radius: var(--m3-textfield-filled-shape) var(--m3-textfield-filled-shape) 0 0;
     pointer-events: none;
     transition: all 200ms;
   }
