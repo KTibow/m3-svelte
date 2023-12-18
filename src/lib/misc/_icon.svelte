37baa1110dd12950d66bcb79876a8ca2cbdddb8d
Kendell R <KTibow@users.noreply.github.com>
2023-12-18 11:58:17 -0600

    fix reactivity on icons

--- src/lib/misc/_icon.svelte
+++ src/lib/misc/_icon.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import type { IconifyIcon } from "@iconify/types";
-  let icon: IconifyIcon;
-  let width: string = "1em";
-  let height: string = "1em";
+  export let icon: IconifyIcon;
+  export let width: string = "1em";
+  export let height: string = "1em";
   let clazz: string | undefined = undefined;
-  export { icon, width, height, clazz as class };
+  export { clazz as class };
 </script>
 
 <svg {width} {height} class={clazz} viewBox="0 0 {icon.width} {icon.height}">
