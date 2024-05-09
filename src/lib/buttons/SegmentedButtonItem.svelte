316ca9243e6bbb806860ea221392e6cfc63b4b1e
not-nullptr <needhelpwithrift@gmail.com>
2024-05-09 12:09:16 +0100

    add cute segmented list animation

--- src/lib/buttons/SegmentedButtonItem.svelte
+++ src/lib/buttons/SegmentedButtonItem.svelte
@@ -4,12 +4,16 @@
   import type { IconifyIcon } from "@iconify/types";
   import iconCheck from "@ktibow/iconset-material-symbols/check";
   import Ripple from "$lib/effects/Ripple.svelte";
+  import { onMount } from "svelte";
+  import { browser } from "$app/environment";
 
   export let display = "flex";
   export let extraOptions: HTMLLabelAttributes = {};
   export let input: string;
-  export let icon: IconifyIcon | undefined = undefined;
-
+  export let icon: IconifyIcon | null = null;
+  let selected = false;
+  let customIcon: HTMLDivElement;
+  let checkIcon: HTMLDivElement;
   let ripple: (e: MouseEvent) => Promise<void>;
 
   const tsRipple = (e: MouseEvent) => {
@@ -17,6 +21,122 @@
     if (el.disabled) return;
     ripple(e);
   };
+
+  const animateCheck = () => {
+    if (!customIcon || !checkIcon) return;
+    // cancel any existing animations
+    customIcon.getAnimations().forEach((a) => {
+      (a.onfinish as any)();
+      a.cancel();
+    });
+    if (selected) {
+      customIcon.style.width = "1.125rem";
+      customIcon.style.position = "absolute";
+      customIcon.style.left = "16px";
+      customIcon.animate(
+        [
+          {
+            transform: "rotate(0deg) scale(1)",
+            opacity: 1,
+          },
+          {
+            transform: "rotate(90deg) scale(0)",
+            opacity: 0,
+          },
+        ],
+        {
+          duration: 200,
+          easing: "ease",
+        },
+      ).onfinish = () => {
+        customIcon.style.position = "";
+        customIcon.style.width = "";
+      };
+      checkIcon.animate(
+        [
+          {
+            transform: "rotate(-90deg) scale(0)",
+            opacity: 0,
+          },
+          {
+            transform: "rotate(0deg) scale(1)",
+            opacity: 1,
+          },
+        ],
+        {
+          duration: 200,
+          easing: "ease",
+        },
+      );
+    } else {
+      checkIcon.style.width = "1.125rem";
+      checkIcon.style.position = "absolute";
+      checkIcon.style.left = "16px";
+      checkIcon.animate(
+        [
+          {
+            transform: "rotate(0deg)",
+            opacity: 1,
+          },
+          {
+            transform: "rotate(-45deg)",
+            opacity: 0,
+          },
+        ],
+        {
+          duration: 200,
+          easing: "ease",
+        },
+      ).onfinish = () => {
+        checkIcon.style.position = "";
+        checkIcon.style.width = "";
+      };
+
+      customIcon.animate(
+        [
+          {
+            transform: "rotate(45deg)",
+            opacity: 0,
+          },
+          {
+            transform: "rotate(0deg)",
+            opacity: 1,
+          },
+        ],
+        {
+          duration: 200,
+          easing: "ease",
+        },
+      );
+    }
+  };
+
+  onMount(() => {
+    const el = document.getElementById(input) as HTMLInputElement;
+    selected = el.checked;
+    const click = (e: MouseEvent) => {
+      const prev = selected;
+      if (e.target instanceof HTMLInputElement) {
+        // if this is us and its checked, set to true
+        // else, false
+        if (e.target.id === el.id) {
+          selected = e.target.checked;
+        } else {
+          selected = false;
+        }
+        if (selected !== prev) animateCheck();
+      }
+    };
+    const items = el.parentElement!.querySelectorAll("input");
+    items.forEach((item) => {
+      item.addEventListener("click", click);
+    });
+    return () => {
+      items.forEach((item) => {
+        item.removeEventListener("click", click);
+      });
+    };
+  });
 </script>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
@@ -32,11 +152,11 @@
   <div class="layer" />
   <div class="pad" />
   {#if icon}
-    <div class="custom icon">
+    <div class="custom icon" bind:this={customIcon}>
       <Icon {icon} />
     </div>
   {/if}
-  <div class="check icon">
+  <div class="check icon" bind:this={checkIcon} style="z-index: 50;">
     <Icon icon={iconCheck} />
   </div>
   <slot />
