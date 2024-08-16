1398490324ebf70729911f9c871c47c9f3930f14
KTibow <KTibow@users.noreply.github.com>
2024-08-15 18:01:36 -0700

    fix incorrect title width

--- src/routes/docs/Snippet.svelte
+++ src/routes/docs/Snippet.svelte
@@ -42,7 +42,7 @@
 
   p {
     background-color: rgb(var(--m3-scheme-surface-container-highest));
-    margin: -1rem 0 1rem -1rem;
+    margin: -1rem -1rem 1rem -1rem;
     padding: 0.5rem 1rem;
   }
   pre {
