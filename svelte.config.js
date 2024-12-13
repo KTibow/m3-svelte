d55f774fd381234a5a69dff478bb388e94a3b888
KTibow <KTibow@users.noreply.github.com>
2024-12-12 18:26:17 -0800

    no more relative

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+    paths: {
+      relative: false,
+    },
+  },
 };
 
 export default config;
