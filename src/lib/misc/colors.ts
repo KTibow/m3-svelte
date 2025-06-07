040aed5c5211d2bf4eaa575bc0a8d1840ca709f4
KTibow <KTibow@users.noreply.github.com>
2025-06-06 19:48:58 -0700

    subtle containers

--- src/lib/misc/colors.ts
+++ src/lib/misc/colors.ts
@@ -12,8 +12,57 @@ const onOnPrimary = DynamicColor.fromPalette({
   background: () => materialColors.onPrimary(),
   contrastCurve: () => new ContrastCurve(6, 6, 7, 11),
 });
+// Custom colors for you to use
+const primaryContainerSubtle = DynamicColor.fromPalette({
+  name: "primary_container_subtle",
+  palette: (s) => s.primaryPalette,
+  isBackground: true,
+  background: (s) => materialColors.highestSurface(s),
+  contrastCurve: () => undefined,
+});
+const onPrimaryContainerSubtle = DynamicColor.fromPalette({
+  name: "on_primary_container_subtle",
+  palette: (s) => s.primaryPalette,
+  background: () => primaryContainerSubtle,
+  contrastCurve: () => new ContrastCurve(6, 6, 7, 11),
+});
+const tertiaryContainerSubtle = DynamicColor.fromPalette({
+  name: "tertiary_container_subtle",
+  palette: (s) => s.tertiaryPalette,
+  isBackground: true,
+  background: (s) => materialColors.highestSurface(s),
+  contrastCurve: () => undefined,
+});
+const onTertiaryContainerSubtle = DynamicColor.fromPalette({
+  name: "on_tertiary_container_subtle",
+  palette: (s) => s.tertiaryPalette,
+  background: () => tertiaryContainerSubtle,
+  contrastCurve: () => new ContrastCurve(6, 6, 7, 11),
+});
+const errorContainerSubtle = DynamicColor.fromPalette({
+  name: "error_container_subtle",
+  palette: (s) => s.errorPalette,
+  isBackground: true,
+  background: (s) => materialColors.highestSurface(s),
+  contrastCurve: () => undefined,
+});
+const onErrorContainerSubtle = DynamicColor.fromPalette({
+  name: "on_error_container_subtle",
+  palette: (s) => s.errorPalette,
+  background: () => errorContainerSubtle,
+  contrastCurve: () => new ContrastCurve(6, 6, 7, 11),
+});
 
-export const colors = [...materialColors.allColors, onOnPrimary];
+export const colors = [
+  ...materialColors.allColors,
+  onOnPrimary,
+  primaryContainerSubtle,
+  onPrimaryContainerSubtle,
+  tertiaryContainerSubtle,
+  onTertiaryContainerSubtle,
+  errorContainerSubtle,
+  onErrorContainerSubtle,
+];
 
 export const pairs = [
   [materialColors.primary(), materialColors.onPrimary()],
