57e0844fac69c1bc88f6dfed5435d440d2e85155
MrSproutt <84256826+mrsproutt@users.noreply.github.com>
2025-08-22 18:59:29 -0600

    non spec colors  for better contrast

--- src/lib/nav/NavigationRailItem.svelte
+++ src/lib/nav/NavigationRailItem.svelte
@@ -53,7 +53,7 @@
     gap: 4px;
     font-size: 12px;
     user-select: none;
-    color: rgb(var(--m3-scheme-secondary));
+    color: rgb(var(--m3-scheme-on-surface-variant));
     width: fit-content;
     position: relative;
   }
