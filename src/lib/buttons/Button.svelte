795984f6e955be66e0c04596ce04b7428c906ba5
KTibow <KTibow@users.noreply.github.com>
2025-08-26 09:01:07 -0700

    label/link/button chip

--- src/lib/buttons/Button.svelte
+++ src/lib/buttons/Button.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-  import type {
-    HTMLButtonAttributes,
-    HTMLAnchorAttributes,
-    HTMLLabelAttributes,
-  } from "svelte/elements";
+  import type { HTMLButtonAttributes } from "svelte/elements";
   import type { Snippet } from "svelte";
+  import type { LabelAttrs, AnchorAttrs } from "$lib/misc/typing-utils";
   import Layer from "$lib/misc/Layer.svelte";
 
   // If you want a toggle button, use `for` with a checkbox input.
-  type ActionProps =
-    | ({ for: string } & HTMLLabelAttributes)
-    | ({ href: string } & HTMLAnchorAttributes)
-    | HTMLButtonAttributes;
+  type ActionProps = LabelAttrs | AnchorAttrs | HTMLButtonAttributes;
   type Props = {
     variant?: "elevated" | "filled" | "tonal" | "outlined" | "text";
     square?: boolean;
