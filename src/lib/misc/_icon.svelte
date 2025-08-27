5b9f1905d82d8c8d8b67510e27319fc7e3532dfb
daimond113 <contact@daimond113.com>
2025-08-27 18:45:41 +0200

    Icons

--- src/lib/misc/_icon.svelte
+++ src/lib/misc/_icon.svelte
@@ -6,14 +6,22 @@
     width = "1em",
     height = "1em",
     class: clazz,
+    ...props
   }: {
     icon: IconifyIcon;
     width?: string;
     height?: string;
     class?: string;
-  } = $props();
+  } & Partial<{ "aria-label": string; "aria-labelledby": string }> = $props();
 </script>
 
-<svg {width} {height} class={clazz} viewBox="0 0 {icon.width} {icon.height}">
+<svg
+  {width}
+  {height}
+  class={clazz}
+  viewBox="0 0 {icon.width} {icon.height}"
+  aria-hidden={!props["aria-label"] && !props["aria-labelledby"]}
+  {...props}
+>
   {@html icon.body}
 </svg>
