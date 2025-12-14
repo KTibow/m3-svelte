9fe4fbf859e8ebe2d9716e57b0913f93d8d3ae69
KTibow <KTibow@users.noreply.github.com>
2025-12-14 08:18:19 -0800

    fix linking when / in name

--- src/routes/_doc.svelte
+++ src/routes/_doc.svelte
@@ -14,7 +14,10 @@
   } = $props();
 
   let demoURL = $derived(
-    `https://github.com/KTibow/m3-svelte/blob/main/src/demos.md#${name.toLowerCase().replaceAll(" ", "-")}`,
+    `https://github.com/KTibow/m3-svelte/blob/main/src/demos.md#${name
+      .toLowerCase()
+      .replaceAll(" ", "-")
+      .replace(/[^a-z0-9-]/g, "")}`,
   );
 </script>
 
