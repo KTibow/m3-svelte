4c76f219c291800c62cc45b895695667ad38799a
KTibow <KTibow@users.noreply.github.com>
2026-01-03 11:43:56 -0800

    sveltekit/vite toggle

--- src/routes/docs/quick-start/+page.svelte
+++ src/routes/docs/quick-start/+page.svelte
@@ -13,6 +13,7 @@
   import Snippet from "../Snippet.svelte";
 
   let fontSelection = $state("gsans");
+  let frameworkSelection = $state("kit");
 </script>
 
 <svelte:head><title>Quick start</title></svelte:head>
@@ -37,16 +38,47 @@
         <Icon icon={iconConfig} />
       </div>
       <div class="text">Enable functions and mixins</div>
+      <ConnectedButtons>
+        <Button variant="tonal" square label
+          ><input
+            type="radio"
+            value="kit"
+            name="framework"
+            bind:group={frameworkSelection}
+          />SvelteKit</Button
+        >
+        <Button variant="tonal" square label
+          ><input
+            type="radio"
+            value="vite"
+            name="framework"
+            bind:group={frameworkSelection}
+          />Vite</Button
+        >
+      </ConnectedButtons>
     </h2>
-    <Snippet
-      name="vite.config.ts"
-      html={/* typescript */ `import { defineConfig } from "vite";
+    {#if frameworkSelection == "kit"}
+      <Snippet
+        name="vite.config.ts"
+        html={/* typescript */ `import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { functionsMixins } from "vite-plugin-functions-mixins";
 
 export default defineConfig({
-  plugins: [/* your existing plugins */ functionsMixins({ deps: ["m3-svelte"] })],
+  plugins: [sveltekit(), functionsMixins({ deps: ["m3-svelte"] })],
 });`}
-    />
+      />
+    {:else}
+      <Snippet
+        name="vite.config.ts"
+        html={/* typescript */ `import { defineConfig } from "vite";
+import { functionsMixins } from "vite-plugin-functions-mixins";
+
+export default defineConfig({
+  plugins: [functionsMixins({ deps: ["m3-svelte"] })],
+});`}
+      />
+    {/if}
   </li>
   <li>
     <h2>
@@ -56,12 +88,16 @@ export default defineConfig({
       </div>
       <div class="text">Enable your <a href={resolve("/theme")}>theme snippet</a></div>
     </h2>
-    <Snippet
-      name="+layout.svelte, App.svelte, or similar"
-      html={/* svelte */ `<${""}script>
-  import "../app.css"; // Change this if you're not in SvelteKit
+    {#if frameworkSelection == "kit"}
+      <Snippet
+        name="+layout.svelte"
+        html={/* svelte */ `<${""}script>
+  import "../app.css";
 </${""}script>`}
-    />
+      />
+    {:else}
+      <Snippet name="index.ts" html={/* typescript */ `import "./app.css";`} />
+    {/if}
     <Snippet name="app.css" html={/* css */ `/* Your theme snippet */`} />
   </li>
   <li>
@@ -71,19 +107,19 @@ export default defineConfig({
         <Icon icon={iconType} />
       </div>
       <ConnectedButtons>
-        <Button variant="tonal" square label
-          ><input type="radio" value="gsans" name="font" bind:group={fontSelection} />Enable Google
-          Sans</Button
-        >
-        <Button variant="tonal" square label
-          ><input type="radio" value="manual" name="font" bind:group={fontSelection} />Enable your
-          font</Button
-        >
+        <Button variant="tonal" square label>
+          <input type="radio" value="gsans" name="font" bind:group={fontSelection} />Enable Google
+          Sans
+        </Button>
+        <Button variant="tonal" square label>
+          <input type="radio" value="manual" name="font" bind:group={fontSelection} />Enable a
+          custom font
+        </Button>
       </ConnectedButtons>
     </h2>
     {#if fontSelection == "gsans"}
       <Snippet
-        name="app.html"
+        name={frameworkSelection == "kit" ? "app.html" : "index.html"}
         html={/* html */ `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,400..700&display=swap" />`}
       />
     {:else}
