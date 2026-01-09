ff489187646aaf732a3a35f20e868a5121910133
KTibow <KTibow@users.noreply.github.com>
2026-01-08 17:07:24 -0800

    always use runes

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
+  compilerOptions: { runes: true },
   kit: {
     adapter: adapter(),
     paths: {
