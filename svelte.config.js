017e0496c156a777396a8986eb09d98c1c62e94e
KTibow <KTibow@users.noreply.github.com>
2026-02-11 16:41:53 -0800

    we no longer need vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
   compilerOptions: { runes: true },
   kit: {
     adapter: adapter(),
