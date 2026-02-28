4721ef5f065cf8c6ff6144b484902e20b88420a9
KTibow <KTibow@users.noreply.github.com>
2026-02-27 21:55:07 -0800

    help the ripples render better

--- src/routes/theme/SelectColor.svelte
+++ src/routes/theme/SelectColor.svelte
@@ -14,7 +14,7 @@
   let { showSecondary = false }: { showSecondary?: boolean } = $props();
 </script>
 
-{#snippet palette(bg: string, fg: string)}
+{#snippet palette(bg: string)}
   <svg width="20" height="20" viewBox="0 0 24 24" style="overflow:visible">
     <path
       d="M12 22q-2.05 0-3.875-.788t-3.187-2.15t-2.15-3.187T2 12q0-2.075.813-3.9t2.2-3.175T8.25 2.788T12.2 2q2 0 3.775.688t3.113 1.9t2.125 2.875T22 11.05q0 2.875-1.75 4.413T16 17h-1.85q-.225 0-.312.125t-.088.275q0 .3.375.863t.375 1.287q0 1.25-.687 1.85T12 22m-4.425-9.425Q8 12.15 8 11.5t-.425-1.075T6.5 10t-1.075.425T5 11.5t.425 1.075T6.5 13t1.075-.425m3-4Q11 8.15 11 7.5t-.425-1.075T9.5 6t-1.075.425T8 7.5t.425 1.075T9.5 9t1.075-.425m5 0Q16 8.15 16 7.5t-.425-1.075T14.5 6t-1.075.425T13 7.5t.425 1.075T14.5 9t1.075-.425m3 4Q19 12.15 19 11.5t-.425-1.075T17.5 10t-1.075.425T16 11.5t.425 1.075T17.5 13t1.075-.425M12 20q.225 0 .363-.125t.137-.325q0-.35-.375-.825T11.75 17.3q0-1.05.725-1.675T14.25 15H16q1.65 0 2.825-.962T20 11.05q0-3.025-2.312-5.038T12.2 4Q8.8 4 6.4 6.325T4 12q0 3.325 2.338 5.663T12 20"
@@ -24,7 +24,7 @@
     />
     <path
       d="M12 22q-2.05 0-3.875-.788t-3.187-2.15t-2.15-3.187T2 12q0-2.075.813-3.9t2.2-3.175T8.25 2.788T12.2 2q2 0 3.775.688t3.113 1.9t2.125 2.875T22 11.05q0 2.875-1.75 4.413T16 17h-1.85q-.225 0-.312.125t-.088.275q0 .3.375.863t.375 1.287q0 1.25-.687 1.85T12 22m-4.425-9.425Q8 12.15 8 11.5t-.425-1.075T6.5 10t-1.075.425T5 11.5t.425 1.075T6.5 13t1.075-.425m3-4Q11 8.15 11 7.5t-.425-1.075T9.5 6t-1.075.425T8 7.5t.425 1.075T9.5 9t1.075-.425m5 0Q16 8.15 16 7.5t-.425-1.075T14.5 6t-1.075.425T13 7.5t.425 1.075T14.5 9t1.075-.425m3 4Q19 12.15 19 11.5t-.425-1.075T17.5 10t-1.075.425T16 11.5t.425 1.075T17.5 13t1.075-.425M12 20q.225 0 .363-.125t.137-.325q0-.35-.375-.825T11.75 17.3q0-1.05.725-1.675T14.25 15H16q1.65 0 2.825-.962T20 11.05q0-3.025-2.312-5.038T12.2 4Q8.8 4 6.4 6.325T4 12q0 3.325 2.338 5.663T12 20"
-      fill={fg}
+      fill="currentColor"
     />
   </svg>
 {/snippet}
@@ -32,18 +32,20 @@
   title="Primary source color"
   class="m3-layer color-disc"
   style:background-color={hexFromArgb($sourceColor)}
+  style:color="var(--m3c-on-primary)"
 >
   <input
     type="color"
     bind:value={() => hexFromArgb($sourceColor), (v) => ($sourceColor = argbFromHex(v))}
   />
-  {@render palette("var(--m3c-primary)", "var(--m3c-on-primary)")}
+  {@render palette("var(--m3c-primary)")}
 </label>
 {#if showSecondary}
   <label
     title="Secondary source color"
     class="m3-layer color-disc"
     style:background-color={hexFromArgb($secondarySourceColor)}
+    style:color="var(--m3c-on-tertiary)"
   >
     <input
       type="color"
@@ -51,7 +53,7 @@
         () => hexFromArgb($secondarySourceColor), (v) => ($secondarySourceColor = argbFromHex(v))
       }
     />
-    {@render palette("var(--m3c-tertiary)", "var(--m3c-on-tertiary)")}
+    {@render palette("var(--m3c-tertiary)")}
   </label>
 {/if}
 <Button variant="tonal" iconType="full" title="Pick from an image" label>
